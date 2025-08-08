4da52e710dfcd9f5e4be68a175cf8de52b154fd3
shakeelaBatool <shakeelasadpara@gmail.com>
2025-08-07 22:53:39 -0700

    Add files via upload

---
+++ BodyMassIndex.py
@@ -0,0 +1,11 @@
+def my_fun(a,b):
+    bmi=weight/height**2
+    print(bmi)                  
+   
+    return bmi                                   #return output
+weight=int(input("Enter weight in kg : ")) 
+height=float(input("Enter height in meter: "))
+resent=my_fun(weight,height)                     # function call
+print(resent)    
+
+print(round(resent,2))                           # roundoff 
